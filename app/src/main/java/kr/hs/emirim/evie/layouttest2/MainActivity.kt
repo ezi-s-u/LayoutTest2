b4a710786c6a267348dbaa87ee8b3d3f9d4ad777
ezi-s-u <ljsgoo0617@gmail.com>
2023-05-30 19:26:08 +0900

    코틀린 클래스에서 LinearLayout 생성

--- app/src/main/java/kr/hs/emirim/evie/layouttest2/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/layouttest2/MainActivity.kt
@@ -1,11 +1,19 @@
 package kr.hs.emirim.evie.layouttest2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.self_5_2)
+
+        val linear1 = LinearLayout(this);
+        linear1.orientation = LinearLayout.VERTICAL
+        linear1.setBackgroundColor(Color.rgb(0,51,0))
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        setContentView(linear1, params)
     }
 }
