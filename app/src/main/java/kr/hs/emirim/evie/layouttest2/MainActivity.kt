4ab54a9edd975d1b0c93924e523d5846b1d33db9
ezi-s-u <ljsgoo0617@gmail.com>
2023-05-30 19:13:48 +0900

    직접풀어보기 5-2

---
+++ app/src/main/java/kr/hs/emirim/evie/layouttest2/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.evie.layouttest2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.self_5_2)
+    }
+}
